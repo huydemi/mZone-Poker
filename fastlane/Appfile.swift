79b306924391ac0744281f9e90db79d6f5735dbb
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-27 21:43:10 +0700

    setup

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "com.huydang.mZone-Poker-huysamdua86gmailcom" } // The bundle identifier of your app
+var appleID: String { return "huysamdua86@gmail.com" } // Your Apple email address
+
+var itcTeam: String? { return "119179835" } // App Store Connect Team ID
+var teamID: String { return "AA388QFS75" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
